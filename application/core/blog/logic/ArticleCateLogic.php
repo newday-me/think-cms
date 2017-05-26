1477b90e9cc02cdf276cedaccc3d329a27a24610
newday-me <newday_me@163.com>
2017-05-26 15:46:05 +0800

    修复后台没有文章分类时文章列表页报错的问题

--- application/core/blog/logic/ArticleCateLogic.php
+++ application/core/blog/logic/ArticleCateLogic.php
@@ -90,17 +90,19 @@ class ArticleCateLogic extends Logic
         $tree = $nest['tree'];
         
         $list = [];
-        foreach ($tree[0] as $vo) {
-            $list[] = [
-                'name' => $vo['cate_title'],
-                'value' => $vo['id']
-            ];
-            if (isset($tree[$vo['id']])) {
-                foreach ($tree[$vo['id']] as $ko) {
-                    $list[] = [
-                        'name' => '--' . $ko['cate_title'],
-                        'value' => $ko['id']
-                    ];
+        if (isset($tree[0])) {
+            foreach ($tree[0] as $vo) {
+                $list[] = [
+                    'name' => $vo['cate_title'],
+                    'value' => $vo['id']
+                ];
+                if (isset($tree[$vo['id']])) {
+                    foreach ($tree[$vo['id']] as $ko) {
+                        $list[] = [
+                            'name' => '--' . $ko['cate_title'],
+                            'value' => $ko['id']
+                        ];
+                    }
                 }
             }
         }
